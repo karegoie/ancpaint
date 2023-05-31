60e3cc373b631d433baaf004c420b1c13316a168
karegoie <karegoie@snu.ac.kr>
2023-05-31 10:33:32 +0900

    0531 initial commit

---
+++ include/ancpaint.h
@@ -0,0 +1,8 @@
+//
+// Created by user on 2023-05-30.
+//
+
+#ifndef ANCPAINT_ANCPAINT_H
+#define ANCPAINT_ANCPAINT_H
+
+#endif //ANCPAINT_ANCPAINT_H
---
+++ src/ancpaint.cpp
@@ -0,0 +1,3 @@
+//
+// Created by user on 2023-05-30.
+//
