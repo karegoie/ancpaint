de80fbd3275b1338eb26a502b52fedd3915758da
karegoie <karegoie@snu.ac.kr>
2023-05-31 12:00:22 +0900

    0531 pair reader added

--- include/ancpaint.h
+++ include/ancpaint.h
@@ -5,4 +5,26 @@
 #ifndef ANCPAINT_ANCPAINT_H
 #define ANCPAINT_ANCPAINT_H
 
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+class reader {
+public:
+    static int pair_reader(string& first_file, string& second_file);
+    int bed_reader(string& first_file, string& second_file);
+};
+
+class utils {
+public:
+    int distance_measure;
+    int who_is_first;
+    int generate_report;
+};
+
+
+
+
 #endif //ANCPAINT_ANCPAINT_H
--- src/ancpaint.cpp
+++ src/ancpaint.cpp
@@ -1,3 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include "../include/ancpaint.h"
+
 //
 // Created by user on 2023-05-30.
 //
+using namespace std;
+
+int reader::pair_reader(string& first_file, string& second_file) {
+    vector <pair<string, string>> gene_pair;
+
+    string line;
+    ifstream file("./pair.txt");
+    if(file.is_open()){
+        unsigned int oe = 0;
+        while(getline(file, line, '\t')) {
+            string one, two;
+            if(oe % 2 == 0) {
+                one = line;
+
+            } else {
+                two = line;
+            }
+            pair<string, string> p = make_pair(one, two);
+            gene_pair.push_back(p);
+            oe++;
+        }
+        file.close();
+    } else {
+        cout << "Unable to open file" << endl;
+        return 1;
+    }
+
+    return 0;
+}
+
+int reader::bed_reader(string& first_file, string& second_file) {
+
+    return 0;
+}
